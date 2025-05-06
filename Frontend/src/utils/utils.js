8256e96c57ef7008a699239408b0acd016bbc11e
Arijit Bhowmik <122301763+ARIJIT0483@users.noreply.github.com>
2025-05-06 10:29:51 +0530

    Update BAckend URL

--- Frontend/src/utils/utils.js
+++ Frontend/src/utils/utils.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "https://code-stack-backend.vercel.app/api/v1"
+// export const BACKEND_URL = "https://code-stack-backend.vercel.app/api/v1"
+export const BACKEND_URL = "https://codestack.onrender.com/api/v1"
